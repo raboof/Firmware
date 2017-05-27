289e96d93d9e0f05663741e95200b145a5af6dbd
Bas van Sisseren <bas@vansisseren.nl>
2017-05-27 13:48:00 +0200

    add gpio-pin support;
    can only configure gpio pins as output atm. still needs some work.

--- components/badge/badge_mpr121.h
+++ components/badge/badge_mpr121.h
@@ -5,16 +5,14 @@ typedef void (*badge_mpr121_intr_t)(void*);
 
 extern void badge_mpr121_init(void);
 
-extern int badge_mpr121_set_io_direction(uint8_t pin, uint8_t direction);
-extern int badge_mpr121_set_output_state(uint8_t pin, uint8_t state);
-extern int badge_mpr121_set_output_high_z(uint8_t pin, uint8_t high_z);
-extern int badge_mpr121_set_input_default_state(uint8_t pin, uint8_t state);
-extern int badge_mpr121_set_pull_enable(uint8_t pin, uint8_t enable);
-extern int badge_mpr121_set_pull_down_up(uint8_t pin, uint8_t up);
-extern int badge_mpr121_set_interrupt_enable(uint8_t pin, uint8_t enable);
 extern void badge_mpr121_set_interrupt_handler(uint8_t pin, badge_mpr121_intr_t handler, void *arg);
 
-extern int badge_mpr121_get_input(void);
 extern int badge_mpr121_get_interrupt_status(void);
+
+// FIXME: now hardcoded 'output'
+extern int badge_mpr121_configure_gpio(int pin, int flags);
+
+extern int badge_mpr121_get_gpio_level(int pin);
+extern int badge_mpr121_set_gpio_level(int pin, int value);
 
 #endif // BADGE_MPR121_H
